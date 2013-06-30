3859d97c38cb485be85da63f1d444afb47478d6b
kwando <kwando@merciless.me>
2013-06-30 15:44:13 +0200

    Tool for setting the anistrophic filtering on a texture.

---
+++ src/me/merciless/util/TextureTools.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.merciless.util;
+
+import com.jme3.material.MatParam;
+import com.jme3.material.Material;
+import com.jme3.texture.Texture2D;
+
+/**
+ *
+ * @author kwando
+ */
+public class TextureTools {
+  public static void setAnistropic(Material material, String texturename, int level){
+    MatParam param = material.getParam(texturename);
+    Object object = param.getValue();
+    if(object instanceof Texture2D){
+      Texture2D texture = (Texture2D)object;
+      texture.setAnisotropicFilter(level);
+    }else{
+      System.err.println("tried to set anistropic on a non texture param");
+    }
+  }
+}
